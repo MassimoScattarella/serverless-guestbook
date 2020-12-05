9e494858f96e3e4fc92a564fcbd800d4d8e3d791
Massimo Scattarella <massimo.scattarella@mitobit.com>
2020-12-05 13:47:09 +0100

    fix api

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://CHANGEME.us-south.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://c56dfdb1.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
